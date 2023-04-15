2ebfe6cb4ecb0682c3e75c7a9b629b8f05b4cd42
mauro-balades <mauro.balades@tutanota.com>
2023-04-15 09:27:04 +0200

    Added the main idea

---
+++ rtcm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Start the command and capture its PID
+$@ &         # Execute the command passed as argument in the background
+PID=$!       # Capture the PID of the command (i.e., the PID of the last background process)
+
+# Loop until the command finishes
+while [ -e /proc/$PID ]
+do
+    # Get the CPU usage and elapsed time
+    CPU=$(ps -p $PID -o %cpu | tail -n 1)     # Use the "ps" command to get the CPU usage percentage of the process with the given PID
+    ELAPSED=$(ps -p $PID -o etime | tail -n 1) # Use the "ps" command to get the elapsed time (in the format "DD-HH:MM:SS") of the process with the given PID
+
+    # Get the memory usage
+    MEM=$(ps -p $PID -o rss | tail -n 1)       # Use the "ps" command to get the resident set size (RSS) of the process with the given PID
+
+    # Display the information
+    echo "CPU usage: $CPU"      # Print the CPU usage percentage
+    echo "Elapsed time: $ELAPSED" # Print the elapsed time
+    echo "Memory usage: $MEM"   # Print the memory usage
+
+    # Wait for a second before checking again
+    sleep 1      # Pause the script for 1 second before checking again
+done
