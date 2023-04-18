88bdbe2d01593dbfe186ef8f97d95a6fa3b6ddaa
mauro-balades <mauro.balades@tutanota.com>
2023-04-18 19:51:52 +0200

    Added info and dialog for instructions

--- rtcm.sh
+++ rtcm.sh
@@ -4,6 +4,8 @@
 $@ &> log &  # Execute the command passed as argument in the background
 PID=$!       # Capture the PID of the command (i.e., the PID of the last background process)
 
+touch info
+
 # Loop until the command finishes
 while [ -e /proc/$PID ]
 do
@@ -15,14 +17,21 @@ do
     MEM=$(ps -p $PID -o rss | tail -n 1)       # Use the "ps" command to get the resident set size (RSS) of the process with the given PID
 
     # Display the information
-    echo "CPU usage: $CPU"      # Print the CPU usage percentage
-    echo "Elapsed time: $ELAPSED" # Print the elapsed time
-    echo "Memory usage: $MEM"   # Print the memory usage
+    truncate -s 0 info
+    echo "CPU usage: $CPU" >> info      # Print the CPU usage percentage
+    echo "Elapsed time: $ELAPSED" >> info # Print the elapsed time
+    echo "Memory usage: $MEM" >> info   # Print the memory usage
 
     # Wait for a second before checking again
-    sleep 1      # Pause the script for 1 second before checking again
-done | dialog --begin 7 5  --no-shadow --title "Log" --tailboxbg log 25 130 \
-              --and-widget \
-              --begin 1 5 --no-shadow --title "Progress" --gauge "" 5 130 0 && \
+    # sleep 0.1      # Pause the script for 1 second before checking again
+done | dialog --begin 7 5 --title "Log" --no-shadow --tailboxbg log 25 65 \
+        --and-widget \
+        --begin 7 75 --no-shadow --title "Information" --tailboxbg info 12 65 \
+        --and-widget \
+        --begin 20 75 --msgbox "Click [CTRL+C] (for definitive exit) or \"Accept\" when program finished\n\nwarning: If you click \"Accept\" and the program is not finished, you will encounter an empty box, just ctrl+c if you want to definitively exit without the program finishing" 12 65
 
+dialog --clear --title "Result" --no-shadow --msgbox "Program finished successfully." 10 65
+
+echo ""
 rm log
+rm info
